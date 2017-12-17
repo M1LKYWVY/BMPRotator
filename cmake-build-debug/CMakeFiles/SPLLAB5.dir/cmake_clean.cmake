5fb71c21444c5e8460d196c9dbefc78d303e741a
= <=>
2017-12-17 13:37:04 +0300

    Bug fixes. Rotation works fine (theoretically).

---
+++ cmake-build-debug/CMakeFiles/SPLLAB5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SPLLAB5.dir/main.c.o"
+  "CMakeFiles/SPLLAB5.dir/bmp.c.o"
+  "SPLLAB5.pdb"
+  "SPLLAB5.exe"
+  "libSPLLAB5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SPLLAB5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include "bmp.h"
+
+int main(int argc, char *argv[]) {
+
+    if(argc!=2){
+        fprintf(stderr, "%s\n", "Usage: source.bmp");
+        return 1;
+    }
+
+    FILE *input = fopen(argv[1], "rb");
+
+    if (!input)
+    {
+        strerror(errno);
+        return 2;
+    }
+
+
+
+    image_t *image = malloc(sizeof(image_t));
+    read_status error_code = from_bmp(input,image);
+
+    switch(error_code) {
+
+        case READ_OK : {
+            puts("File was successfully read!");
+        } break;
+
+        case READ_INVALID_BITS : {
+            fprintf(stderr, "%s\n", "Invalid bits!");
+            return 4;
+        }
+
+        case READ_INVALID_HEADER : {
+            fprintf(stderr, "%s\n", "Invalid file header!");
+            return 5;
+        }
+
+        case READ_INVALID_SIGNATURE : {
+            fprintf(stderr, "%s\n", "Invalid signature!");
+            return 6;
+        }
+    }
+    char type;
+    printf("Type in which side it will be rotated (\"r\" or \"l\"): ");
+    scanf("%c", &type);
+    if (type=='r') {
+        puts("rr");
+    }
+    else {
+        puts("ll");
+    }
+    FILE *outptr = fopen(argv[1], "wb+");
+    if (!outptr)
+    {
+        strerror(errno);
+        return 3;
+    }
+
+    image_t rotated = rotate_90(*image, type);
+    write_status write_status = to_bmp(outptr,&rotated);
+    free(rotated.data);
+
+    switch (write_status) {
+
+        case WRITE_OK : {
+            puts("File was successfully written!");
+            if (type=='r') {
+                puts("Rotated on 90 degrees right.");
+            }
+            else {
+                puts("Rotated on 90 degrees left.");
+            }
+            break;
+        }
+
+        case WRITE_ERROR : {
+            fprintf(stderr, "%s\n", "Error occurred while writing!");
+            return 7;
+        }
+
+    }
+    return 0;
+}
